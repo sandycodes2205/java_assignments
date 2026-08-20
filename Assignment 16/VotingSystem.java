3be9682305b3621a6c17031b4cc0113318cd59bb
sandycodes-1502 <torkadsandesh@gmail.com>
2026-08-20 21:35:45 +0530

    Assignment 16

---
+++ Assignment 16/VotingSystem.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class InvalidAgeException extends Exception {
+    InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+public class VotingSystem {
+
+    static void checkAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age is below 18. Not eligible for voting.");
+        }
+
+        System.out.println("You are eligible for voting.");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter your age: ");
+            int age = sc.nextInt();
+
+            checkAge(age);
+
+        } catch (InvalidAgeException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        sc.close();
+    }
+}
